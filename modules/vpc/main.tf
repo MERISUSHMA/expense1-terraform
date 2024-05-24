cc7cd306706b928aefba18a50c3da50f816d6913
HP <ammuraj4466@gmail.com>
2024-05-24 16:13:09 +0530

    terraform variables

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,5 +8,6 @@ resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets[count.index]
   tags       = merge(var.tags, {Name = "public_subnet"})
+  availability_zone = var.azs[count.insex]
 }
 
--- vars.tf
+++ vars.tf
@@ -5,3 +5,5 @@ variable "public_subnets" {}
 variable "web_subnets" {}
 variable "app_subnets" {}
 variable "db_subnets" {}
+
+variable "azs" {}
