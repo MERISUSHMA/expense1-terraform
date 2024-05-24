de46e5c19966fc4b6cecc6627c2529ba503a3536
HP <ammuraj4466@gmail.com>
2024-05-24 18:17:46 +0530

    terraform variables

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -13,6 +13,7 @@ resource "aws_subnet" "public" {
   tags       = merge(var.tags, {Name = "public_subnet"})
   availability_zone = var.azs[count.index]
 }
+
 resource "aws_subnet" "web" {
   count      = length(var.web_subnets)
   vpc_id = aws_vpc.main.id
@@ -20,6 +21,7 @@ resource "aws_subnet" "web" {
   tags = merge(var.tags, {Name = "web_subnet"})
   availability_zone = var.azs[count.index]
 }
+
 resource "aws_subnet" "app" {
   count = length(var.app_subnets)
   vpc_id = aws_vpc.main.id
@@ -27,43 +29,54 @@ resource "aws_subnet" "app" {
   tags = merge(var.tags, {Name = "app_subnet"})
   availability_zone = var.azs[count.index]
 }
-resource "aws_subnet" "db" {
+
+resourc e "aws_subnet" "db" {
   count = length(var.db_subnets)
   vpc_id = aws_vpc.main.id
   cidr_block = var.db_subnets[count.index]
   tags = merge(var.tags, {Name = "db_subnet"})
   availability_zone = var.azs[count.index]
 }
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, {Name = "public"})
 }
+
 resource "aws_route_table" "web" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, {Name = "web"})
 }
+
 resource "aws_route_table" "app" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, {Name = "app"})
 }
+
 resource "aws_route_table" "db" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, {Name = "db"})
 }
+
 resource "aws_route_table_association" "public" {
   count = length(aws_subnet.public)
   subnet_id      = aws_subnet.public.*.id[count.index]
   route_table_id = aws_route_table.public.id
 }
+
 resource "aws_route_table_association" "web" {
   count = length(aws_subnet.web)
   subnet_id      = aws_subnet.web.*.id[count.index]
   route_table_id = aws_route_table.web.id
-}resource "aws_route_table_association" "app" {
+}
+
+resource "aws_route_table_association" "app" {
   count = length(aws_subnet.app)
   subnet_id      = aws_subnet.app.*.id[count.index]
   route_table_id = aws_route_table.app.id
-}resource "aws_route_table_association" "db" {
+}
+
+resource "aws_route_table_association" "db" {
   count = length(aws_subnet.db)
   subnet_id      = aws_subnet.db.*.id[count.index]
   route_table_id = aws_route_table.db.id
