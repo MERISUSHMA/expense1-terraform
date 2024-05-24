ce1d71816e8c3e2a5f21c71d4f0e508209e7c0b9
HP <ammuraj4466@gmail.com>
2024-05-24 15:55:14 +0530

    terraform variables

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,8 +4,9 @@ resource "aws_vpc" "main" {
 
 }
 resource "aws_subnet" "public" {
-  count      = lenth(var.public_subnets)
+  count      = length(var.public_subnets)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets[count.index]
+  tags       = merge(var.tags, {Name = "public_subnet"})
 }
 
